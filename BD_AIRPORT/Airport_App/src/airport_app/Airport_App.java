1ffb756d690d93ee687a751522dc53726f89e2c5
Nenoxx <nenoxxcraft3@hotmail.fr>
2017-10-08 19:08:37 +0200

    added Java Project

---
+++ BD_AIRPORT/Airport_App/src/airport_app/Airport_App.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airport_app;
+
+/**
+ *
+ * @author nenoxx
+ */
+public class Airport_App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Airport_Login al = new Airport_Login();
+        al.setVisible(true);
+    }
+    
+}
---
+++ BD_AIRPORT/Airport_App/src/database/utilities/IFileReader.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database.utilities;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+
+/**
+ *
+ * @author nenoxx
+ */
+public class IFileReader {
+    private String csvFile = "";
+    private String line = "";
+    private Boolean continuer = true;
+    
+    public IFileReader(String path){
+        csvFile = path;
+    }
+    
+    public String GetProperty(String propertyName){
+        String value = "";
+        
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null && continuer == true) {
+                String[] r = line.split(" = ");
+                if(r[0].compareTo(propertyName) == 0){ //iz ok
+                    value = r[1];
+                    continuer = false;
+                }
+            }
+            if(continuer == false)//Alors on a trouvé
+                return value;
+            else return null;
+        }
+        catch(Exception ex){
+            System.out.println("Han ouais " + ex.getLocalizedMessage());
+        } 
+        return null;
+    }
+    
+    public String FetchRow(String key, String separator){
+        String value = "";
+        
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null && continuer == true) {
+                String[] r = line.split(separator);
+                if(r[0].compareTo(key) == 0){ //iz ok
+                    value = r[0] + separator + r[1];
+                    continuer = false;
+                }
+            }
+            if(continuer == false)//Alors on a trouvé
+                return value;
+            else return null;
+        }
+        catch(Exception ex){
+            System.out.println("Han ouais " + ex.getLocalizedMessage());
+        } 
+        return null;
+    }
+    
+}
